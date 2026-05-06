12d026160e75f542fa1d348fe96fc737ee094b50
Ali Tavallaie <a.tavallaie@gmail.com>
2026-05-06 22:58:30 +0400

    update SITEURL to point to the correct GitHub Pages URL

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://diversity-and-inclusion-wg.example.com"
+SITEURL = "https://psf.github.io/diversity-and-inclusion-wg"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
